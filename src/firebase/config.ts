d5cd8238c7d6d7740bcc5430a9a85773faab25a5
Hưng Triệu <hungtrieu2910@gmail.com>
2026-01-10 07:50:25 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2722908522-a0a28",
+  "appId": "1:120185625122:web:25e41b5e712e4b302f487b",
+  "apiKey": "AIzaSyDy8Si6uEgTS1CyMpCxBzQTmIjr2-VOUuY",
+  "authDomain": "studio-2722908522-a0a28.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "120185625122"
+};
